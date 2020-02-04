bf3ea0eb0b840775ad058b906ec00d8e9abec8c5
Titas <titasdg@gmail.com>
2020-02-04 13:29:05 +0200

    first commit

--- resources/views/skelbimai/_partials/nav.blade.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <li><a href="#">Linkas1</a></li>
-    <li><a href="#">Linkas2</a></li>
-    <li><a href="#">Linkas3</a></li>
-    <li><a href="#">Linkas4</a></li>
-    <li><a href="#">Linkas5</a></li>
-    <li><a href="#">Linkas6</a></li>
-</nav>
