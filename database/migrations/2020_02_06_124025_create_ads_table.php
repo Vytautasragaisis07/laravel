8b164ebf50a7b4e5c9f12c0b421824e26dae4a77
Titas <titasdg@gmail.com>
2020-02-06 15:13:00 +0200

    lietuviškai

---
+++ database/migrations/2020_02_06_124025_create_ads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('category_id');
+            $table->longText('description');
+            $table->decimal('price');
+            $table->string('img')-> nullable();
+            $table->string('email');
+            $table->string('phone')-> nullable();
+            $table->string('location');
+            $table->integer('catid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
